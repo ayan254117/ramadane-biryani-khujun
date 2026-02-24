c7afb7d19551c134d5405d4ca803af2e925ca5e0
Ayan <admissionoffice.su@gmail.com>
2026-02-24 10:53:20 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh' // অথবা আপনার আগের ইম্পোর্টটি ঠিক রাখুন
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
-    allowedHosts: ['ramadane-biryani-khujun.onrender.com']
+    allowedHosts: true
   }
 })
